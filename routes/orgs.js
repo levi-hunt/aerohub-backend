a18055438e59b25142c24defa411bdfb2ca7a4eb
Levi Hunt <levi.hunt08@gmail.com>
2024-06-19 16:56:49 +1000

    Took old swaggerUI out

--- routes/orgs.js
+++ routes/orgs.js
@@ -8,49 +8,7 @@ const bcrypt = require('bcrypt');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
-/**
- * @swagger
- *  tags:
- *      name: Organisation
- *      description: All CRUD operations that can be performed on the orgs table.
- */
-
-
-/**
- * @swagger
- * /org/createOrg:
- *  post:
- *      summary: Create a new organisation
- *      description: Creates a new organisation with an org id, name and contact email
- *      tags:
- *          - Organisation
- *      requestBody:
- *          required: true
- *          content:
- *              application/json:
- *                  schema:
- *                      type: object
- *                      properties:
- *                          name:
- *                              type: string
- *                              example: Aviarc
- *                          contact_email:
- *                              type: string
- *                              example: levihunt@proton.me
- *                      required:
- *                          - name
- *                          - contact_email
- *      responses:
- *          201:
- *              description: Organisation created successfully
- *          400:
- *              description: Invalid Input
- *          500:
- *              description: Internal Server Error
- *              
- */
 router.post('/createOrg', async (req, res) => {
-    // Add validation middleware    
     const { name, contact_email } = req.body;
     const createOrg = await prisma.organisations.create({
         data: {
@@ -61,24 +19,6 @@ router.post('/createOrg', async (req, res) => {
     res.json(createOrg);
 })
 
-/**
- * @swagger
- * /org/{org_id}:
- *   get:
- *      tags: 
- *          - Organisation
- *      summary: Returns org id
- *      responses:
- *          200:
- *              description: Successfully printed "The org at id {org_id}"
- *      parameters:
- *          - name: org_id
- *            in: path
- *            schema:
- *              type: integer
- *            required: true
- *            description: Org Iid
- */
 router.get('/:id', async (req, res) => {
     const getOrg = await prisma.organisations.findFirst({
         where: { org_id: Number(req.params.id) }
--- routes/users.js
+++ routes/users.js
@@ -8,54 +8,6 @@ const bcrypt = require('bcrypt');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
-/**
- * @swagger
- *  tags:
- *      name: Users
- *      description: All CRUD operations that can be performed on the users table.
- */
-
-
-/**
- * @swagger
- * /user/createUser:
- *   post:
- *     summary: Create a new user
- *     description: Creates a new user with the given first name, last name, and email.
- *     tags:
- *       - Users
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               first_name:
- *                 type: string
- *                 example: John
- *               last_name:
- *                 type: string
- *                 example: Doe
- *               primary_email:
- *                 type: string
- *                 example: john.doe@example.com
- *               password:
- *                  type: string
- *                  example: Aerohub123
- *             required:
- *               - first_name
- *               - last_name
- *               - primary_email
- *               - password
- *     responses:
- *       201:
- *         description: User created successfully
- *       400:
- *         description: Invalid input
- *       500:
- *         description: Internal Server Error
- */
 router.post('/createUser', async (req, res) => {
     const { first_name, last_name, primary_email, password } = req.body;
 
@@ -85,24 +37,6 @@ router.post('/createUser', async (req, res) => {
     }
 });
 
-/**
- * @swagger
- * /user/{userId}:
- *   get:
- *      tags: 
- *          - Users
- *      summary: Returns user id
- *      responses:
- *          200:
- *              description: Successfully printed "Your id is {id}"
- *      parameters:
- *          - name: userId
- *            in: path
- *            schema:
- *              type: integer
- *            required: true
- *            description: User Id
- */
 router.get('/:id', async (req, res) => {
     const getUser = await prisma.users.findFirst({
         where: { user_id: Number(req.params.id) }
