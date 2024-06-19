bf8a4334ac04c10714522d5923c505443b5b0353
Levi Hunt <levi.hunt08@gmail.com>
2024-06-19 16:56:01 +1000

    Moved all swaggerui stuff into 1 file

--- server.js
+++ server.js
@@ -2,12 +2,11 @@
 
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerDocument = require('./swagger.json');
 
 // Import Routing
 const userRoutes = require('./routes/users.js');
-const orgRoutes = require('./routes/orgs.js')
+const orgRoutes = require('./routes/orgs.js');
 
 const app = express();
 const port = 3000;
@@ -16,28 +15,12 @@ app.use(express.json());
 app.use('/user', userRoutes);
 app.use('/org', orgRoutes);
 
-const swaggerOptions = {
-    swaggerDefinition: swaggerDocument,
-    apis: ['./server.js', 'routes/users.js', 'routes/orgs.js'],
-};
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-
-/**
- * @swagger
- * /:
- *   get:
- *     summary: Returns the homepage
- *     responses:
- *       200:
- *         description: Successfully returned SwaggerDocs HTML
- */
 app.get('/', (req, res) => {
     res.redirect('/api');
 });
 
 app.listen(port, () => {
-
+    console.log(`Server is running on port ${port}`);
 });
