2727d5a126d83d59925f7413fbb22787f1885c38
Levi Hunt <levi.hunt08@gmail.com>
2024-06-20 23:59:42 +1000

    Added basic route scaffolding for orgs

--- routes/organisations.js
+++ routes/organisations.js
@@ -2,9 +2,23 @@
 const express = require('express');
 const router = express.Router();
 
-// Define your organization routes here
-router.get('/', (req, res) => {
-    res.send('Organization route');
-});
+router.route('/:id')
+    .get(async (req, res) => {
+        res.send("Single org ID here");
+    })
+    .put(async (req, res) => {
+        res.send("This updates an unique org");
+    })
+    .delete(async (req, res) => {
+        res.send("This will delete an unique org");
+    })
+
+router.route('/')
+    .get(async (req, res) => {
+        res.send("Returns every org");
+    })
+    .post(async (req, res) => {
+        res.send("This creates an org");
+    })
 
 module.exports = router;
